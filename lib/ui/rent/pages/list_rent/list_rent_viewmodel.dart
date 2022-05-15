6a55e4b8352bb8abc111fdf1f7820b3c0d9a388f
Carlos Diaz <carlos.cedp@gmail.com>
2022-05-14 21:51:02 -0400

    🚧

--- lib/ui/rent/pages/list_rent/list_rent_viewmodel.dart
+++ lib/ui/rent/pages/list_rent/list_rent_viewmodel.dart
@@ -1,3 +1,72 @@
+import 'package:car_rental_app/domain/models/rent.dart';
+import 'package:car_rental_app/domain/repository/rent_repository_abstract.dart';
 import 'package:stacked/stacked.dart';
 
-class ListRentViewModel extends BaseViewModel {}
+class ListRentViewModel extends BaseViewModel {
+  RentRepositoryAbstract repository;
+  List<Rent>? _list;
+  List<Rent>? get list => _list;
+  List<String> columnNames = [
+    'Id',
+    'Empleado',
+    'Vehiculo',
+    'Cliente',
+    'Renta',
+    'Devolucion',
+    'Monto x Dia',
+    'Cant. Dias',
+    'Estado',
+    'Acciones'
+  ];
+  ListRentViewModel({
+    required this.repository,
+  });
+
+  Future<void> loadData() async {
+    clearErrors();
+    setBusy(true);
+    var res = await repository.getAll();
+    res.fold((ex) {
+      setError(ex);
+    }, (data) {
+      _list = data;
+    });
+    setBusy(false);
+  }
+
+  Future<bool> create(Rent data) async {
+    clearErrors();
+    var res = await repository.create(data);
+    var response = false;
+    res.fold((ex) {
+      setError(ex);
+    }, (data) {
+      response = data;
+    });
+    return response;
+  }
+
+  Future<bool> update(Rent data) async {
+    clearErrors();
+    var res = await repository.update(data);
+    var response = false;
+    res.fold((ex) {
+      setError(ex);
+    }, (data) {
+      response = data;
+    });
+    return response;
+  }
+
+  Future<bool> delete(int id) async {
+    clearErrors();
+    var res = await repository.delete(id);
+    var response = false;
+    res.fold((ex) {
+      setError(ex);
+    }, (data) {
+      response = data;
+    });
+    return response;
+  }
+}
